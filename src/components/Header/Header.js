37fe713de6aaa4ede8f294485abf860f5dac8c70
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-09-28 21:44:17 +0600

    Header Done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGuitar } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Boost Your Passion</h1>
+            <h1><FontAwesomeIcon icon={faGuitar}></FontAwesomeIcon> Boost Your Creativity, Build Your Passion...</h1>
         </div >
     );
 };
